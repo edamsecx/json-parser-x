dda2d6b1b844a7f0f7b78844ed7cd89d8a9845e1
edamsecx <161575830+edamsecx@users.noreply.github.com>
2024-03-19 12:57:44 +0000

    add cache

--- benchmark/index.ts
+++ benchmark/index.ts
@@ -1,51 +1,71 @@
 import Benchmark from "benchmark";
-import { jsonParser } from "..";
-import { toJSON } from "flatted"; 
+import { jsonCacheParser, jsonParser } from "..";
+import { toJSON } from "flatted";
 import { OldJSONParser } from "./old";
 
 const suite = new Benchmark.Suite();
 
 const exData = JSON.stringify({
-    name: "Amex",
-    age: 14,
-    isX: true,
-    information: {
-        github: "@EdamAme-x",
-        gitlab: null,
-        twitter: "@amex2189"
-    },
-    achievement: ["SuperX", true, {}]
+  name: "Amex",
+  age: 14,
+  isX: true,
+  information: {
+    github: "@EdamAme-x",
+    gitlab: null,
+    twitter: "@amex2189",
+  },
+  achievement: ["SuperX", true, {}],
 });
 
 const naitive = () => {
-    return JSON.parse(exData);
-}
+  return JSON.parse(exData);
+};
 
 const self = () => {
-    return jsonParser(exData);
-}
+  return jsonParser(exData);
+};
 
 const flatted = () => {
-    return toJSON(exData);
-}
+  return toJSON(exData);
+};
 
 const old = () => {
-    return OldJSONParser(exData);
-}
+  return OldJSONParser(exData);
+};
 
-suite.on('start', () => {
-    console.log('[Benchmark Start]')
-  }).add('Naitive JSON Parser', () => {
-    naitive()
-  }).add('Self-made JSON Parser', () => {
-    self()
-  }).add('Flatted JSON Parser', () => {
-    flatted()
-  }).add('Old JSON Parser', () => {
-    old()
-  }).on('cycle', (event: Event) => {
-    console.log(String(event.target))
-  }).on('complete', function () {
-    // @ts-expect-error
-    console.log(`[Fastest]\n${this.filter('fastest').map('name').map((name, index) => ` ${index + 1}. ${name}`).join("\n")}\n ...`)
-  }).run({ async: true })
+const cacheParser = new jsonCacheParser();
+
+const cache = () => {
+  return cacheParser.parse(exData);
+};
+
+suite
+  .on("start", () => {
+    console.log("[Benchmark Start]");
+  })
+  .add("Naitive JSON Parser", () => {
+    naitive();
+  })
+  .add("Self-made JSON Parser", () => {
+    self();
+  })
+  .add("Flatted JSON Parser", () => {
+    flatted();
+  })
+  .add("Old JSON Parser", () => {
+    old();
+  })
+  .add("Self-made Cache JSON Parser", () => {})
+  .on("cycle", (event: Event) => {
+    console.log(String(event.target));
+  })
+  .on("complete", function () {
+    console.log(
+      //@ts-expect-error
+      `[Fastest]\n${this.filter("fastest")
+        .map("name")
+        .map((name: string, index: number) => ` ${index + 1}. ${name}`)
+        .join("\n")}\n ...`,
+    );
+  })
+  .run({ async: true });
--- index.ts
+++ index.ts
@@ -16,3 +16,39 @@ export function jsonParser<T = any>(jsonString: string): T {
 
   return JSON.parse(jsonString);
 }
+
+export class jsonCacheParser {
+  cache: Map<string, any> = new Map();
+  cacheKeyList: string[] = [];
+
+  constructor(public maxCacheSize: number = Infinity) {
+    if (this.maxCacheSize <= 0) {
+      throw new Error("maxCacheSize must be greater than 0");
+    }
+  }
+
+  parse<T = any>(jsonString: string): T {
+    if (this.cache.has(jsonString)) {
+      return this.cache.get(jsonString);
+    } else {
+      const result = jsonParser<T>(jsonString);
+      if (this.cache.size >= this.maxCacheSize) {
+        const firstKey = this.cacheKeyList.shift();
+        if (firstKey) this.cache.delete(firstKey);
+      }
+      const cacheKey = this.toHash(jsonString);
+      this.cache.set(cacheKey, result);
+      this.cacheKeyList.push(cacheKey);
+      return result;
+    }
+  }
+
+  toHash(jsonString: string): string {
+    let a = 5381;
+    for (let c = jsonString.length; c--; )
+      (a += jsonString.charCodeAt(c)), (a += a << 10), (a ^= a >> 6);
+    a += a << 3;
+    a ^= a >> 11;
+    return (((a + (a << 15)) & 4294967295) >>> 0).toString(16);
+  }
+}
